d9def7cf4610b0e84db84e71ef4f90159a71907b
eleanorlaw <eleanor.law@dtc.ox.ac.uk>
2016-11-14 15:59:48 +0000

    added eleanor.h as it was confused for as renaming a different file before

---
+++ src/score/eleanor.h
@@ -0,0 +1,52 @@
+// -*- mode: c++; c-file-style: "ellemtel"; encoding: utf-8; -*-
+//
+// Copyright (C) 2012 Saulo H.P. de Oliveira
+//
+// Saulo H. P. de Oliveira <saulo.deoliveira@pmb.ox.ac.uk>
+// 2012-08-06
+//
+
+#ifndef ELEANOR_INCLUDED
+#define ELEANOR_INCLUDED
+
+class Peptide;
+/**
+ * 
+ * Saulo's Scoring Class is a very intuitive class:
+ *
+ * Add a random score component to represent noise.
+ * 
+ */
+class Eleanor
+{
+public:
+	// Constructor
+	Eleanor(); 
+	// Destructor
+	~Eleanor();
+
+	/* This method returns the random Score for the Peptide! */
+	double score(const Peptide& peptide, bool verbose = false);
+
+	// Set the name of the Span data file
+	void set_short_data_file(const std::string &filename);
+	void set_long_data_file(const std::string &filename);
+
+private:
+	void load_data(const Peptide& p);
+	//
+private:
+	std::string m_filename;
+	bool m_data_loaded;					// whether data has been loaded
+	int *m_con[2],num_con;
+	// number of spans, read from span file
+	int m_num_spans;
+	// a vector of span starts and a vector of span ends
+	//int *m_spans[2];
+	// a vector of whether each residue is in a span
+	bool m_layer[1000];
+	int *m_satisfied_con;
+	int m_previous_len;
+};
+
+#endif // ELEANOR_INCLUDED
